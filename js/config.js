c9117520a2c9db4904071e57f42a4dc1c3b8063d
Elton Jung <eltonjung@example.com>
2026-04-22 19:17:17 -0300

    Deploy: Atualizando URL do Ngrok para teste de fumaça

--- js/config.js
+++ js/config.js
@@ -6,7 +6,7 @@ const CONFIG = {
     // 1. AO REINICIAR O PC:
     //    Rode o comando 'ngrok http 5000'
     //    Copie a nova URL gerada e cole abaixo:
-    API_BASE: "http://localhost:5000", 
+    API_BASE: "https://4fa0-2804-2b60-11b3-a201-80ba-9e2f-bad4-35ef.ngrok-free.app", 
 
     // 2. WHATSAPP DE ATENDIMENTO:
     WHATSAPP_CONTATO: "5551999906748"
